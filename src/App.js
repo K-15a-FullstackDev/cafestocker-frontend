8e34e341cfa800f1ecc14648845c62ff8abf847c
Kushal <kushal1053@gmail.com>
2020-05-05 09:20:00 -0500

    chore(frontend): init CRA app shell for CafeStocker

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{ maxWidth: 720, margin: "2rem auto", fontFamily: "sans-serif" }}
+    >
+      <h1>CafeStocker</h1>
+      <p>Inventory tracker</p>
+      <hr />
+      <p>UI running. We’ll add items list & form next.</p>
+    </div>
+  );
+}
+export default App;
